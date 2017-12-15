b39a077a947b31247372335132b3abb70cad3fe2
Hogan McDonald <hpeter.mcdonald@icloud.com>
2017-12-15 15:50:03 -0600

    first pzzle day 2 solved

--- modules/day02/Day02.js
+++ modules/day02/Day02.js
@@ -11,8 +11,22 @@ module.exports = class Day02 {
   }
 
   static solve() {
+    let result = 0;
     let spreadsheet = this.loadSpreadsheet();
-    return spreadsheet;
+
+    for (var i = 0; i < spreadsheet.length; i++) {
+      let row = spreadsheet[i];
+      let largestValue = row[0];
+      let smallestValue = row[0];
+      for (var j = 1; j < row.length; j++) {
+        row[i]
+        largestValue = (row[j] > largestValue) ? row[j] : largestValue;
+        smallestValue = (row[j] < smallestValue) ? row[j] : smallestValue;
+      }
+      result += largestValue - smallestValue;
+    }
+
+    return result;
   }
 
 };
