4e95725b34a665aae6e9e3963da99f50021faba2
Hogan McDonald <hpeter.mcdonald@icloud.com>
2017-12-15 12:45:06 -0600

    day 1 solved

--- modules/day01/Day01.js
+++ modules/day01/Day01.js
@@ -4,8 +4,18 @@ module.exports = class Day01 {
 
   static solve() {
     let result = 0;
-    result = Utility.parseInput(1);
+    let captcha = Utility.parseInput(1).split('').map( str => Number(str));
+    captcha.pop();
+
+    for (let i = 0; i <= captcha.length - 1; i++) {
+      let num = captcha[i];
+      let nextNum = (i < captcha.length - 1) ? captcha[i + 1] : captcha[0];
+      if (num === nextNum) {
+        result += num;
+      }
+    }
+
     return result;
   }
-  
+
 };
