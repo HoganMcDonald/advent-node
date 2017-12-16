607a3cddbb502753006809f4222940bab137deae
Hogan McDonald <hpeter.mcdonald@icloud.com>
2017-12-15 22:07:28 -0600

    day 4 both complete

---
+++ modules/days/day04/Day04.js
@@ -0,0 +1,32 @@
+const Utility = require('../../tools/Utility.js');
+
+module.exports = class Day04 {
+
+  static _loadPassphrases() {
+    let passphrases = Utility.parseInput(4).split('\n');
+    passphrases.pop();
+    return passphrases;
+  }
+
+  static solve() {
+    let passphrases = this._loadPassphrases();
+
+    return passphrases.reduce( (s, passphrase) => {
+  		let passwords = passphrase.split(' ')
+  		return s + passwords.reduce( ( accumulator, currentValue, currentIndex, array ) => {
+        return accumulator && array.indexOf(currentValue) == currentIndex
+      })
+    }, 0 );
+  }
+
+  static bonus() {
+    let passphrases = this._loadPassphrases();
+
+    return passphrases.reduce( (s, passphrase) => {
+  		let passwords = passphrase.split(' ').map( a => ( a.split('').sort().join('') ) );
+  		return s + passwords.reduce( ( accumulator, currentValue, currentIndex, array ) => {
+        return accumulator && array.indexOf(currentValue) == currentIndex
+      })
+    }, 0 );
+  }
+};
